7a660f7eb19b9b8404e1bf1c9586e6fc67b83ef8
fancaixing <18770335251@163.com>
2019-07-07 12:39:24 +0800

    修改spring版本

--- src/main/java/com/fcx/controller/UserController.java
+++ src/main/java/com/fcx/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.fcx.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.fcx.pojo.User;
 import com.fcx.service.IUserService;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +22,14 @@ import javax.servlet.http.HttpServletRequest;
 public class UserController {
     @Resource
     private IUserService userService;
+    private static Logger logger = Logger.getLogger(UserController.class);
 
     @RequestMapping("/showUser")
     public String toIndex(HttpServletRequest request, Model model){
         int userId = Integer.parseInt(request.getParameter("id"));
         User user = this.userService.getUserById(userId);
         model.addAttribute("user", user);
+        logger.info(JSON.toJSONString(user));
         return "showUser";
     }
 }
